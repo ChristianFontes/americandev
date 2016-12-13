5320c1c1a5040762d8000e0c6a871a0e507e89ae
Christian Fontes <christianfontes82@gmail.com>
2016-12-13 21:47:33 +0100

    add firebase

---
+++ src/components/firebase.js
@@ -0,0 +1,11 @@
+import * as firebase from "firebase";
+
+var Firebase = {
+	apiKey: "AIzaSyDXCRNFdkcqtjidJu07bRGm49AGywsl5Go",
+	authDomain: "americandev-d94ae.firebaseapp.com",
+	databaseURL: "https://americandev-d94ae.firebaseio.com",
+	storageBucket: "americandev-d94ae.appspot.com",
+	messagingSenderId: "633459966617"
+};
+
+export default Firebase;
--- src/views/App.js
+++ src/views/App.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import * as firebase from "firebase";
+
+//Import CSS
 import '../css/App.css';
+
 //Import Components
 import AppBar from '../components/AppBar';
 import Table from '../components/Table';
+import Firebase from '../components/firebase';
+
+firebase.initializeApp(Firebase);
+
+var defaultDatabase = firebase.database();
+console.log(defaultDatabase);
 
 class App extends Component {
   render() {
